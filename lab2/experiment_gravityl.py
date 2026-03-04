9e4297581f838dc82a4792be6f91a0c352378465
Abdirahman Sudi <Abdirahmansudi122@gmail.com>
2026-03-04 14:05:06 +0100

    Update experiment_gravityl.py

--- lab2/experiment_gravityl.py
+++ lab2/experiment_gravityl.py
@@ -1,12 +1,14 @@
 """
+
 In this experiment file we will showcase the gravity mechanic of our simulation.
-The grav is modeled as a attractive force, proportional to the ditance between partiucles radius
+The gravity is modeled as a attractive force, proportional to the distance between particles radius
 In order to see clearly, one of the particles will be highlighted with color and a vel vector.
 
-The model behaves as expected, however it does often go towards a form of cahos. When the
-particles gets to close or the G cinstant is to large, their veolicties increase to the point
-that tracking them on the screen becomes impossible. From  a vel of around 10 units / dt to 
-well over 2000. 
+
+The model behaves as expected, however it does often go towards a form of chaos. When the
+particles gets to close or the G constant is too large, their velocities increase to the point
+that tracking them on the screen becomes impossible. From  a vel of around 10 units / dt to
+well over 2000.
 """
 
 
@@ -27,12 +29,12 @@ for i in range(n):
 # create the walls of the simulation
 walls(canvas,particles[0].radius) 
 
-def forces(dt,particles):
+def forces(dt,particles): # the forces that will act
    gravitational_force(dt,particles,500)
    wall_collision(particles,Vec(-1,0),Vec(8,0))
    wall_collision(particles,Vec(1,0),Vec(-8,0))
    wall_collision(particles,Vec(0,-1),Vec(0,8))
    wall_collision(particles,Vec(0,1),Vec(0,-8))
 
-simulation_loop(forces, 0.0001, particles)
+simulation_loop(forces, 0.0001, particles) # run simulation
 
